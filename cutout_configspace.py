c9cfdf603c6ecd44db4eace36158f1eabf8dee6e
misganazemo@gmail.com <misgananegassi@gmail.com>
2018-03-29 15:40:38 +0200

    prep:optimize cutout with model-based hyperband

---
+++ cutout_configspace.py
@@ -0,0 +1,157 @@
+import json
+import sys
+import os
+import argparse
+
+import ConfigSpace as CS
+from os.path import abspath, join as path_join
+
+import hpbandster
+import hpbandster.distributed.utils
+from hpbandster.distributed.worker import Worker
+from hpbandster.config_generators.kde_ei import KDEEI
+
+from train import run_cutout
+
+def get_config_space(seed=None):
+    # XXX: Change lower upper and default
+    cs = CS.ConfigurationSpace(seed)
+    HPs = [
+        CS.UniformIntegerHyperparameter("length", lower=1, upper=64, default_value=2),
+        CS.UniformIntegerHyperparameter("n_holes", lower=1, upper=32, default_value=16),
+    ]
+
+    [cs.add_hyperparameter(hp) for hp in HPs]
+
+    return cs
+
+
+def main():
+    # XXX: Insert hyperband stuff here
+
+    parser = argparse.ArgumentParser(description='Simple python script to run experiments on augmented data using random search')
+
+
+
+    parser.add_argument(
+        "--model", help="Neural network to be trained with augmented data",
+        default="resnet18"
+    )
+
+    parser.add_argument(
+        "--dataset", help="Dataset to train neural network on",
+        default="cifar10"
+    )
+
+    parser.add_argument(
+        "--max_epochs", default=160, help="Maximum number of epochs to train network", type=int
+    )
+
+    parser.add_argument(
+        "--optimizer", default="hyperband", help="Number of successive halving for hyperband",
+    )
+
+    parser.add_argument(
+        "--run-id", help="The id of single job"
+    )
+
+    parser.add_argument("--seed", help="Random seed.", default=1)
+
+    args = parser.parse_args()
+
+    config_space = get_config_space(seed=args.seed)
+
+    # this run hyperband sequentially
+
+    class CutoutWorker(Worker):
+        def __init__(self, model="resnet18", dataset="cifar10", *args, **kwargs):
+            self.model = model
+            self.dataset = dataset
+            super().__init__(*args, **kwargs)
+
+        def compute(self, config, budget, *args, **kwargs):
+            """
+            Simple example for a compute function
+
+            The loss is just a the config + some noise (that decreases with the budget)
+            There is a 10 percent failure probability for any run, just to demonstrate
+            the robustness of Hyperband agains these kinds of failures.
+            """
+
+            results= run_cutout(
+                epochs=int(budget), model=self.model, dataset=self.dataset,
+                cutout=True, data_augmentation=True,
+                n_holes=config["n_holes"], length=config["length"]
+            )
+            test_error = results["test_error"]
+
+            return({
+                'loss': test_error,   # this is the a mandatory field to run hyperband
+                'info': results  # can be used for any user-defined information - also mandatory
+            })
+
+
+    # starts a local nameserve
+    from hpbandster.distributed import utils as distributed_utils
+    nameserver, ns_port = distributed_utils.start_local_nameserver()
+
+
+    # import the definition of the worker (could be in here as well, but is imported to reduce code duplication)
+
+    # starting the worker in a separate thread
+    w = CutoutWorker(
+        dataset=args.dataset, model=args.model,
+        nameserver=nameserver, ns_port=ns_port,
+    )
+    w.run(background=True)
+
+    # simple config space here: just one float between 0 and 1
+
+    if args.optimizer == "BOHB":
+        print("Using Model Based Hyperband")
+        CG = KDEEI(config_space, mode="sampling", num_samples=64)  # model-based hyperband
+    else:
+        print("Using Hyperband")
+        CG = hpbandster.config_generators.RandomSampling(config_space)  # hyperband on steriods
+
+
+    # instantiating Hyperband with some minimal configuration
+    HB = hpbandster.HB_master.HpBandSter(
+        config_generator=CG,
+        run_id='0',
+        eta=2,
+        min_budget=10,
+        max_budget=args.max_epochs,
+        nameserver=nameserver,
+        ns_port=ns_port,
+        job_queue_sizes=(0, 1)
+    )
+    # runs one iteration if at least one worker is available, first parameter
+    # is number of successive halving
+    res = HB.run(5, min_n_workers=1)
+    # shutdown the worker and the dispatcher
+    HB.shutdown(shutdown_workers=True)
+
+    # Save results
+    path = path_join(abspath("."), "AutoData/{}/cutout".format(args.dataset))
+
+    # Get important information about best configuration from HB result object
+    best_config_id = res.get_incumbent_id()  # Config_id of the incumbent with smallest loss
+    best_run = res.get_runs_by_id(best_config_id)[-1]
+    best_config_trajectory = res.get_incumbent_trajectory()
+
+
+    json_data = {
+        "best_config_id": best_config_id,
+        "best_run_info": best_run.info,
+        "best_config_trajectory": best_config_trajectory
+    }
+
+    # XXX: change output path to be in the arguements
+
+    with open(os.path.join(path, "{}_{}_{}.json".format(args.optimizer, args.dataset, args.run_id)), "w") as fh:
+        json.dump(json_data, fh)
+
+
+if __name__ == "__main__":
+    main()
--- train.py
+++ train.py
@@ -22,215 +22,244 @@ from util.cutout import Cutout
 from model.resnet import ResNet18
 from model.wide_resnet import WideResNet
 
-model_options = ['resnet18', 'wideresnet']
-dataset_options = ['cifar10', 'cifar100', 'svhn']
 
-parser = argparse.ArgumentParser(description='CNN')
-parser.add_argument('--dataset', '-d', default='cifar10',
-                    choices=dataset_options)
-parser.add_argument('--model', '-a', default='resnet18',
-                    choices=model_options)
-parser.add_argument('--batch_size', type=int, default=128,
-                    help='input batch size for training (default: 128)')
-parser.add_argument('--epochs', type=int, default=200,
-                    help='number of epochs to train (default: 20)')
-parser.add_argument('--learning_rate', type=float, default=0.1,
-                    help='learning rate')
-parser.add_argument('--data_augmentation', action='store_true', default=False,
-                    help='augment data by flipping and cropping')
-parser.add_argument('--cutout', action='store_true', default=False,
-                    help='apply cutout')
-parser.add_argument('--n_holes', type=int, default=1,
-                    help='number of holes to cut out from image')
-parser.add_argument('--length', type=int, default=8,
-                    help='length of the holes')
-parser.add_argument('--no-cuda', action='store_true', default=False,
-                    help='enables CUDA training')
-parser.add_argument('--seed', type=int, default=0,
-                    help='random seed (default: 1)')
+def run_cutout(dataset="cifar10", model="resnet18", epochs=200, batch_size=128,
+        learning_rate=0.1, data_augmentation=False,
+        cutout=False,
+        n_holes=1, length=8,
+        no_cuda=False, seed=0):
+    cuda = not no_cuda and torch.cuda.is_available()
+    cudnn.benchmark = True  # Should make training should go faster for large models
 
-args = parser.parse_args()
-args.cuda = not args.no_cuda and torch.cuda.is_available()
-cudnn.benchmark = True  # Should make training should go faster for large models
+    torch.manual_seed(seed)
+    if cuda:
+        torch.cuda.manual_seed(seed)
 
-torch.manual_seed(args.seed)
-if args.cuda:
-    torch.cuda.manual_seed(args.seed)
+    test_id = dataset + '_' + model
 
-test_id = args.dataset + '_' + args.model
+    # Image Preprocessing
+    if dataset == 'svhn':
+        normalize = transforms.Normalize(mean=[x / 255.0 for x in[109.9, 109.7, 113.8]],
+                                         std=[x / 255.0 for x in [50.1, 50.6, 50.8]])
+    else:
+        normalize = transforms.Normalize(mean=[x / 255.0 for x in [125.3, 123.0, 113.9]],
+                                         std=[x / 255.0 for x in [63.0, 62.1, 66.7]])
 
-print args
-
-# Image Preprocessing
-if args.dataset == 'svhn':
-    normalize = transforms.Normalize(mean=[x / 255.0 for x in[109.9, 109.7, 113.8]],
-                                     std=[x / 255.0 for x in [50.1, 50.6, 50.8]])
-else:
-    normalize = transforms.Normalize(mean=[x / 255.0 for x in [125.3, 123.0, 113.9]],
-                                     std=[x / 255.0 for x in [63.0, 62.1, 66.7]])
-
-train_transform = transforms.Compose([])
-if args.data_augmentation:
-    train_transform.transforms.append(transforms.RandomCrop(32, padding=4))
-    train_transform.transforms.append(transforms.RandomHorizontalFlip())
-train_transform.transforms.append(transforms.ToTensor())
-train_transform.transforms.append(normalize)
-if args.cutout:
-    train_transform.transforms.append(Cutout(n_holes=args.n_holes, length=args.length))
+    train_transform = transforms.Compose([])
+    if data_augmentation:
+        train_transform.transforms.append(transforms.RandomCrop(32, padding=4))
+        train_transform.transforms.append(transforms.RandomHorizontalFlip())
+    train_transform.transforms.append(transforms.ToTensor())
+    train_transform.transforms.append(normalize)
+    if cutout:
+        train_transform.transforms.append(Cutout(n_holes=n_holes, length=length))
 
 
-test_transform = transforms.Compose([
-    transforms.ToTensor(),
-    normalize])
+    test_transform = transforms.Compose([
+        transforms.ToTensor(),
+        normalize])
 
-if args.dataset == 'cifar10':
-    num_classes = 10
-    train_dataset = datasets.CIFAR10(root='data/',
-                                     train=True,
-                                     transform=train_transform,
-                                     download=True)
+    if dataset == 'cifar10':
+        num_classes = 10
+        train_dataset = datasets.CIFAR10(root='data/',
+                                         train=True,
+                                         transform=train_transform,
+                                         download=True)
 
-    test_dataset = datasets.CIFAR10(root='data/',
-                                    train=False,
-                                    transform=test_transform,
-                                    download=True)
-elif args.dataset == 'cifar100':
-    num_classes = 100
-    train_dataset = datasets.CIFAR100(root='data/',
-                                      train=True,
+        test_dataset = datasets.CIFAR10(root='data/',
+                                        train=False,
+                                        transform=test_transform,
+                                        download=True)
+    elif dataset == 'cifar100':
+        num_classes = 100
+        train_dataset = datasets.CIFAR100(root='data/',
+                                          train=True,
+                                          transform=train_transform,
+                                          download=True)
+
+        test_dataset = datasets.CIFAR100(root='data/',
+                                         train=False,
+                                         transform=test_transform,
+                                         download=True)
+    elif dataset == 'svhn':
+        num_classes = 10
+        train_dataset = datasets.SVHN(root='data/',
+                                      split='train',
                                       transform=train_transform,
                                       download=True)
 
-    test_dataset = datasets.CIFAR100(root='data/',
-                                     train=False,
+        extra_dataset = datasets.SVHN(root='data/',
+                                      split='extra',
+                                      transform=train_transform,
+                                      download=True)
+
+        # Combine both training splits (https://arxiv.org/pdf/1605.07146.pdf)
+        data = np.concatenate([train_dataset.data, extra_dataset.data], axis=0)
+        labels = np.concatenate([train_dataset.labels, extra_dataset.labels], axis=0)
+        train_dataset.data = data
+        train_dataset.labels = labels
+
+        test_dataset = datasets.SVHN(root='data/',
+                                     split='test',
                                      transform=test_transform,
                                      download=True)
-elif args.dataset == 'svhn':
-    num_classes = 10
-    train_dataset = datasets.SVHN(root='data/',
-                                  split='train',
-                                  transform=train_transform,
-                                  download=True)
 
-    extra_dataset = datasets.SVHN(root='data/',
-                                  split='extra',
-                                  transform=train_transform,
-                                  download=True)
+    # Data Loader (Input Pipeline)
+    train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                               batch_size=batch_size,
+                                               shuffle=True,
+                                               pin_memory=True,
+                                               num_workers=2)
 
-    # Combine both training splits (https://arxiv.org/pdf/1605.07146.pdf)
-    data = np.concatenate([train_dataset.data, extra_dataset.data], axis=0)
-    labels = np.concatenate([train_dataset.labels, extra_dataset.labels], axis=0)
-    train_dataset.data = data
-    train_dataset.labels = labels
+    test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                              batch_size=batch_size,
+                                              shuffle=False,
+                                              pin_memory=True,
+                                              num_workers=2)
 
-    test_dataset = datasets.SVHN(root='data/',
-                                 split='test',
-                                 transform=test_transform,
-                                 download=True)
+    if model == 'resnet18':
+        cnn = ResNet18(num_classes=num_classes)
+    elif model == 'wideresnet':
+        if dataset == 'svhn':
+            cnn = WideResNet(depth=16, num_classes=num_classes, widen_factor=8,
+                             dropRate=0.4)
+        else:
+            cnn = WideResNet(depth=28, num_classes=num_classes, widen_factor=10,
+                             dropRate=0.3)
 
-# Data Loader (Input Pipeline)
-train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
-                                           batch_size=args.batch_size,
-                                           shuffle=True,
-                                           pin_memory=True,
-                                           num_workers=2)
+    cnn = cnn.cuda()
+    criterion = nn.CrossEntropyLoss().cuda()
 
-test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
-                                          batch_size=args.batch_size,
-                                          shuffle=False,
-                                          pin_memory=True,
-                                          num_workers=2)
 
-if args.model == 'resnet18':
-    cnn = ResNet18(num_classes=num_classes)
-elif args.model == 'wideresnet':
-    if args.dataset == 'svhn':
-        cnn = WideResNet(depth=16, num_classes=num_classes, widen_factor=8,
-                         dropRate=0.4)
+    cnn_optimizer = torch.optim.SGD(cnn.parameters(), lr=learning_rate,
+                                    momentum=0.9, nesterov=True, weight_decay=5e-4)
+
+    if dataset == 'svhn':
+        scheduler = MultiStepLR(cnn_optimizer, milestones=[80, 120], gamma=0.1)
     else:
-        cnn = WideResNet(depth=28, num_classes=num_classes, widen_factor=10,
-                         dropRate=0.3)
-
-cnn = cnn.cuda()
-criterion = nn.CrossEntropyLoss().cuda()
-cnn_optimizer = torch.optim.SGD(cnn.parameters(), lr=args.learning_rate,
-                                momentum=0.9, nesterov=True, weight_decay=5e-4)
-
-if args.dataset == 'svhn':
-    scheduler = MultiStepLR(cnn_optimizer, milestones=[80, 120], gamma=0.1)
-else:
-    scheduler = MultiStepLR(cnn_optimizer, milestones=[60, 120, 160], gamma=0.2)
-
-filename = 'logs/' + test_id + '.csv'
-csv_logger = CSVLogger(args=args, fieldnames=['epoch', 'train_acc', 'test_acc'], filename=filename)
+        scheduler = MultiStepLR(cnn_optimizer, milestones=[60, 120, 160], gamma=0.2)
 
 
-def test(loader):
-    cnn.eval()    # Change model to 'eval' mode (BN uses moving mean/var).
-    correct = 0.
-    total = 0.
-    for images, labels in loader:
-        if args.dataset == 'svhn':
-            # SVHN labels are from 1 to 10, not 0 to 9, so subtract 1
-            labels = labels.type_as(torch.LongTensor()).view(-1) - 1
+    filename = "/beegfs/work/workspace/ws/fr_mn119-augment-0/logs/{}.csv".format(test_id)
+    # filename = 'logs/' + test_id + '.csv'
 
-        images = Variable(images, volatile=True).cuda()
-        labels = Variable(labels, volatile=True).cuda()
+    args = argparse.Namespace(**{
+        "dataset": dataset, "model": model, "epochs": epochs, "batch_size": batch_size,
+        "learning_rate": learning_rate, "data_augmentation": data_augmentation,
+        "cutout": cutout, "n_holes": n_holes, "length": length,
+        "no_cuda": no_cuda, "seed": seed
+    })
 
-        pred = cnn(images)
-
-        pred = torch.max(pred.data, 1)[1]
-        total += labels.size(0)
-        correct += (pred == labels.data).sum()
-
-    val_acc = correct / total
-    cnn.train()
-    return val_acc
+    csv_logger = CSVLogger(args=args, fieldnames=['epoch', 'train_acc', 'test_acc'], filename=filename)
 
 
-for epoch in range(args.epochs):
+    def test(loader):
+        cnn.eval()    # Change model to 'eval' mode (BN uses moving mean/var).
+        correct = 0.
+        total = 0.
+        for images, labels in loader:
+            if dataset == 'svhn':
+                # SVHN labels are from 1 to 10, not 0 to 9, so subtract 1
+                labels = labels.type_as(torch.LongTensor()).view(-1) - 1
 
-    xentropy_loss_avg = 0.
-    correct = 0.
-    total = 0.
+            images = Variable(images, volatile=True).cuda()
+            labels = Variable(labels, volatile=True).cuda()
 
-    progress_bar = tqdm(train_loader)
-    for i, (images, labels) in enumerate(progress_bar):
-        progress_bar.set_description('Epoch ' + str(epoch))
 
-        if args.dataset == 'svhn':
-            # SVHN labels are from 1 to 10, not 0 to 9, so subtract 1
-            labels = labels.type_as(torch.LongTensor()).view(-1) - 1
+            pred = cnn(images)
 
-        images = Variable(images).cuda(async=True)
-        labels = Variable(labels).cuda(async=True)
+            pred = torch.max(pred.data, 1)[1]
+            total += labels.size(0)
+            correct += (pred == labels.data).sum()
 
-        cnn.zero_grad()
-        pred = cnn(images)
+        val_acc = correct / total
+        cnn.train()
+        return val_acc
 
-        xentropy_loss = criterion(pred, labels)
-        xentropy_loss.backward()
-        cnn_optimizer.step()
 
-        xentropy_loss_avg += xentropy_loss.data[0]
+    for epoch in range(epochs):
 
-        # Calculate running average of accuracy
-        _, pred = torch.max(pred.data, 1)
-        total += labels.size(0)
-        correct += (pred == labels.data).sum()
-        accuracy = correct / total
+        xentropy_loss_avg = 0.
+        correct = 0.
+        total = 0.
 
-        progress_bar.set_postfix(
-            xentropy='%.3f' % (xentropy_loss_avg / (i + 1)),
-            acc='%.3f' % accuracy)
+        progress_bar = tqdm(train_loader)
+        for i, (images, labels) in enumerate(progress_bar):
+            progress_bar.set_description('Epoch ' + str(epoch))
 
-    test_acc = test(test_loader)
-    tqdm.write('test_acc: %.3f' % (test_acc))
+            if dataset == 'svhn':
+                # SVHN labels are from 1 to 10, not 0 to 9, so subtract 1
+                labels = labels.type_as(torch.LongTensor()).view(-1) - 1
 
-    scheduler.step(epoch)
+            images = Variable(images).cuda(async=True)
+            labels = Variable(labels).cuda(async=True)
 
-    row = {'epoch': str(epoch), 'train_acc': str(accuracy), 'test_acc': str(test_acc)}
-    csv_logger.writerow(row)
+            cnn.zero_grad()
+            pred = cnn(images)
 
-torch.save(cnn.state_dict(), 'checkpoints/' + test_id + '.pt')
-csv_logger.close()
+            xentropy_loss = criterion(pred, labels)
+            xentropy_loss.backward()
+            cnn_optimizer.step()
+
+            xentropy_loss_avg += xentropy_loss.data[0]
+
+            # Calculate running average of accuracy
+            _, pred = torch.max(pred.data, 1)
+            total += labels.size(0)
+            correct += (pred == labels.data).sum()
+            accuracy = correct / total
+
+            progress_bar.set_postfix(
+                xentropy='%.3f' % (xentropy_loss_avg / (i + 1)),
+                acc='%.3f' % accuracy)
+
+        test_acc = test(test_loader)
+        tqdm.write('test_acc: %.3f' % (test_acc))
+
+        scheduler.step(epoch)
+
+        row= {'epoch': str(epoch), 'train_acc': str(accuracy), 'test_acc': str(test_acc)}
+        csv_logger.writerow(row)
+
+
+    # torch.save(cnn.state_dict(), 'checkpoints/' + test_id + '.pt')
+    csv_logger.close()
+
+    results= {'epoch': epoch, 'train_error': 1- accuracy, 'test_error': 1-test_acc}
+
+    # validation error for hyperband
+    return results
+
+def main():
+    model_options = ['resnet18', 'wideresnet']
+    dataset_options = ['cifar10', 'cifar100', 'svhn']
+
+    parser = argparse.ArgumentParser(description='CNN')
+    parser.add_argument('--dataset', '-d', default='cifar10',
+                        choices=dataset_options)
+    parser.add_argument('--model', '-a', default='resnet18',
+                        choices=model_options)
+    parser.add_argument('--batch_size', type=int, default=128,
+                        help='input batch size for training (default: 128)')
+    parser.add_argument('--epochs', type=int, default=200,
+                        help='number of epochs to train (default: 20)')
+    parser.add_argument('--learning_rate', type=float, default=0.1,
+                        help='learning rate')
+    parser.add_argument('--data_augmentation', action='store_true', default=False,
+                        help='augment data by flipping and cropping')
+    parser.add_argument('--cutout', action='store_true', default=False,
+                        help='apply cutout')
+    parser.add_argument('--n_holes', type=int, default=1,
+                        help='number of holes to cut out from image')
+    parser.add_argument('--length', type=int, default=8,
+                        help='length of the holes')
+    parser.add_argument('--no-cuda', action='store_true', default=False,
+                        help='enables CUDA training')
+    parser.add_argument('--seed', type=int, default=0,
+                        help='random seed (default: 1)')
+
+    args = parser.parse_args()
+    run(**args.__dict__)
+
+if __name__ == "__main__":
+    main()
